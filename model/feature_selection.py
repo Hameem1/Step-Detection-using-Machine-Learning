71e3ba4bf241d9ec341be6ea44bde61198471b05
Hameem <hameemahmed.1@gmail.com>
2019-03-10 07:54:01 +0100

    Added a normalization function, documentation, global variables. Removed class weights

--- model/feature_selection.py
+++ model/feature_selection.py
@@ -16,6 +16,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import chi2, SelectKBest, RFE
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, f1_score, recall_score
+from model.feature_selection_cv import normalize
 
 # Globals
 # list of all feature labels + StepLabel
@@ -23,62 +24,81 @@ cols = pd.read_csv(f'{new_sensor_paths[0]}\\{os.listdir(new_sensor_paths[0])[0]}
 # Setting numpy print precision
 np.set_printoptions(precision=5)
 # no. of best features to select
-n_features = 13
+n_features = 15
 # no. of different Random Forests participating in vote
-r = 3
+r = 5
 # Voting Box
 nth_rank = {k: 0 for k in cols[0:-1]}
 # Features obtained after feature selection
 selected_features = []
 # no. of rows of dataset to be used
 row_count = 50000
+# no. of Decision Trees per Random Forest
+RF_ESTIMATORS = 100
+# If True, the dataset is normalized before training
+DATA_NORMALIZATION = True
+# If True, a selected portion of the entire dataset is used for training
+DATA_REDUCE = True
+# If True, Feature Selection is performed. If False, selected_features has to be manually initialized
+FEATURE_SELECTION = True
 
 # starting timer
 start = time()
-# loading in the actual dataset for one sensor
+# loading in the entire actual dataset for one sensor
 print(DATA_PATH)
 DATA = pd.read_csv(DATA_PATH, sep='\t', index_col=0)
 # Loading the relevant data
-DATA = DATA.iloc[0:row_count, :]
+if DATA_REDUCE:
+    DATA = DATA.iloc[0:row_count, :]
 print('>> Dataset loaded\n')
+# Normalizing the dataset
+if DATA_NORMALIZATION:
+    DATA = normalize(DATA)
+    print('>> Dataset normalized\n')
+# Converting the data to numpy arrays
 data_matrix = DATA.values
+# separating the data into predictors and target
 X = data_matrix[:, 0:-1]
 y = data_matrix[:, -1]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=10)
+if FEATURE_SELECTION:
+    # Splitting the data into training and testing splits
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=10)
 
-# Feature selection
-# Recursive Feature Elimination/Model Based Feature Selection (More powerful than Uni-variate selection)
-model = RandomForestClassifier(n_estimators=100, n_jobs=-1, class_weight={0: 1, 1: 50})
-rfe = RFE(model, n_features)
+    # Feature selection
+    # Recursive Feature Elimination/Model Based Feature Selection (More powerful than Uni-variate selection)
+    model = RandomForestClassifier(n_estimators=100, n_jobs=-1)
+    rfe = RFE(model, n_features)
 
-# Voting between multiple (r) Random forest based rankings
-for i in range(r):
-    print(f'\nGenerating votes from RandomForest # {i+1}')
-    fit = rfe.fit(X_train, y_train)
-    feature_names = list(DATA.columns[0:-1][fit.support_])
-    for feature in feature_names:
-        nth_rank[feature] += 1      # Casting Votes
+    # Voting between multiple (r) Random forest based rankings
+    for i in range(r):
+        print(f'\nGenerating votes from RandomForest # {i+1}')
+        fit = rfe.fit(X_train, y_train)
+        feature_names = list(DATA.columns[0:-1][fit.support_])
+        for feature in feature_names:
+            nth_rank[feature] += 1      # Casting Votes
 
-sorted_nth_rank = sorted(nth_rank.items(), key=operator.itemgetter(1), reverse=True)
-print(f'\n\nSelected {n_features} features:\n')
+    sorted_nth_rank = sorted(nth_rank.items(), key=operator.itemgetter(1), reverse=True)
+    print(f'\n\nSelected {n_features} features:\n')
 
-# Printing Election Results
-count = 1
-for k, v in sorted_nth_rank[0:n_features]:
-    print(f'#{count} -  {k} -   score = {v}/{r}')
-    selected_features.append(k)
-    count += 1
+    # Printing Election Results
+    count = 1
+    for k, v in sorted_nth_rank[0:n_features]:
+        print(f'#{count} -  {k} -   score = {v}/{r}')
+        selected_features.append(k)
+        count += 1
 
-print('>> Feature selection complete\n')
-print(f'Feature selection with "{r}" separate Random Forest Classifiers complete')
-duration = time() - start
-print('Operation took:', f'{duration:.2f} seconds.' if duration < 60 else f'{duration/60:.2f} minutes.\n\n')
+    print('>> Feature selection complete\n')
+    print(f'Feature selection with "{r}" separate Random Forest Classifiers complete')
+    duration = time() - start
+    print('Operation took:', f'{duration:.2f} seconds.' if duration < 60 else f'{duration/60:.2f} minutes.\n\n')
 
+else:
+    print(f'Features being used:\n{selected_features}\n')
 DATA_selected = DATA[selected_features]
 data_matrix = DATA_selected.values
 X_train, X_test, y_train, y_test = train_test_split(data_matrix, y, test_size=0.3, random_state=10)
-model = RandomForestClassifier(n_estimators=100, n_jobs=-1, class_weight={0: 1, 1: 50})
+model = RandomForestClassifier(n_estimators=RF_ESTIMATORS, n_jobs=-1)
 print('>> Training a Random Forest Classifier using the best features\n')
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
--- model/feature_selection_cv.py
+++ model/feature_selection_cv.py
@@ -4,7 +4,6 @@ The model used is a Random Forest classifier.
 The model is trained during the feature selection phase and is tested at the end on test data from the best features.
 
 """
-# Todo: Fix problem with imported data into Subject objects being non numeric
 # Todo: Normalize the data before feeding it to the model (Train+Test)
 # Todo: Adjust the parameters and test feature_selection_cv
 
@@ -20,63 +19,90 @@ from sklearn.feature_selection import chi2, SelectKBest, RFECV
 from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, f1_score, recall_score
 
+
 # Globals
 # list of all feature labels + StepLabel
 cols = pd.read_csv(f'{new_sensor_paths[0]}\\{os.listdir(new_sensor_paths[0])[0]}', sep='\t', index_col=0).columns
 # Setting numpy print precision
 np.set_printoptions(precision=5)
-# Voting Box
-nth_rank = {k: 0 for k in cols[0:-1]}
-# Features obtained after feature selection
-selected_features = []
 # no. of rows of dataset to be used
 row_count = 50000
+# no. of Decision Trees per Random Forest
+RF_ESTIMATORS = 100
+# Performance metric to optimize the model for
+SCORING = 'f1_weighted'
+# If True, the dataset is normalized before training
+DATA_NORMALIZATION = True
+# If True, a selected portion of the entire dataset is used for training
+DATA_REDUCE = True
 
-# starting timer
-start = time()
-# loading in the actual dataset for one sensor
-print(DATA_PATH)
-DATA = pd.read_csv(DATA_PATH, sep='\t', index_col=0)
-# Loading the relevant data
-DATA = DATA.iloc[0:row_count, :]
-print('>> Dataset loaded\n')
-data_matrix = DATA.values
-X = data_matrix[:, 0:-1]
-y = data_matrix[:, -1]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=10)
-# Feature selection
-# Recursive Feature Elimination/Model Based Feature Selection (with cross-validated selection of best # of features)
-model = RandomForestClassifier(n_estimators=100, class_weight={0: 1, 1: 100})
-rfecv = RFECV(estimator=model, cv=StratifiedKFold(2), scoring='f1_weighted', n_jobs=-1)
-print('>> Training model\n')
-fit = rfecv.fit(X_train, y_train)
+def normalize(data):
+    feature_dic = dict()
+    # Performing Min-Max Scaling
+    for feature in cols[:-1]:
+        feature_dic[feature] = list((data[feature] - data[feature].min()) / (data[feature].max() - data[feature].min()))
+    feature_dic['StepLabel'] = data[cols[-1]]
+    df_new = pd.DataFrame.from_dict(feature_dic)
+    return df_new
 
-# summarize ranking
-print(f"# of selected features:     {fit.n_features_}\n")
-feature_names = list(DATA.columns[0:-1][fit.support_])
-print(f"Selected Features:\n{fit.support_}\n{feature_names}\n")
-ranking = pd.DataFrame({'rank': fit.ranking_, 'feature': DATA.columns[0:-1]})
-ranking = ranking.sort_values(by=['rank']).reset_index(drop=True)
-print(f"Feature Ranking:\n{fit.ranking_}\n{ranking}\n")
-print(f"Optimal number of features : {rfecv.n_features_}")
-# Plotting number of features VS. cross-validation scores
-plt.figure()
-plt.xlabel("Number of features selected")
-plt.ylabel("Cross validation score (nb of correct classifications)")
-plt.plot(range(1, len(rfecv.grid_scores_) + 1), rfecv.grid_scores_)
-plt.show()
 
-print('>> Testing model\n')
-print(f'Score of the classifier on test data = {rfecv.score(X_test, y_test)*100:.3f}%\n')
-y_pred = rfecv.predict(X_test)
-print(f'Confusion Matrix:\n{confusion_matrix(y_test, y_pred)}\n')
-print(f'Score of the classifier on test data:\n'
-      f'Accuracy  = {accuracy_score(y_test, y_pred)*100:.3f}%\n'
-      f'Precision = {precision_score(y_test, y_pred)*100:.3f}%\n'
-      f'Recall    = {recall_score(y_test, y_pred)*100:.3f}%\n'
-      f'F1 score  = {f1_score(y_test, y_pred)*100:.3f}%\n\n')
+if __name__ == '__main__':
+    # starting timer
+    start = time()
+    # loading in the entire actual dataset for one sensor
+    print(DATA_PATH)
+    DATA = pd.read_csv(DATA_PATH, sep='\t', index_col=0)
+    # Loading the relevant data (limiting # of rows)
+    if DATA_REDUCE:
+        DATA = DATA.iloc[0:row_count, :]
+    print('>> Dataset loaded\n')
+    # Normalizing the dataset
+    if DATA_NORMALIZATION:
+        DATA = normalize(DATA)
+        print('>> Dataset normalized\n')
+    # Converting the data to numpy arrays
+    data_matrix = DATA.values
+    # separating the data into predictors and target
+    X = data_matrix[:, 0:-1]
+    y = data_matrix[:, -1]
+    # Splitting the data into training and testing splits
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=10)
 
-duration = time() - start
-print('Operation took:', f'{duration:.2f} seconds.' if duration < 60 else f'{duration/60:.2f} minutes.')
+    # Feature selection
+    # Recursive Feature Elimination/Model Based Feature Selection (with cross-validated selection of best # of features)
+    model = RandomForestClassifier(n_estimators=RF_ESTIMATORS)
+    rfecv = RFECV(estimator=model, cv=StratifiedKFold(2), scoring=SCORING, n_jobs=-1)
+    print('>> Training the model\n')
+    fit = rfecv.fit(X_train, y_train)
 
+    # summarize ranking
+    print(f"# of selected features:     {fit.n_features_}/{len(cols[:-1])}\n")
+    feature_names = list(DATA.columns[0:-1][fit.support_])
+    print(f"Selected Features:\n{fit.support_}\n{feature_names}\n")
+    ranking = pd.DataFrame({'rank': fit.ranking_, 'feature': DATA.columns[0:-1]})
+    ranking = ranking.sort_values(by=['rank']).reset_index(drop=True)
+    print(f"Feature Ranking:\n{fit.ranking_}\n{ranking}\n")
+    print(f"Optimal number of features : {rfecv.n_features_}")
+    # Plotting number of features VS. cross-validation scores
+    plt.figure()
+    plt.xlabel("Number of features selected")
+    plt.ylabel("Cross validation score (nb of correct classifications)")
+    plt.plot(range(1, len(rfecv.grid_scores_) + 1), rfecv.grid_scores_)
+    plt.show()
+
+    print('>> Testing model\n')
+    print(f'Score of the classifier on test data = {rfecv.score(X_test, y_test)*100:.3f}%\n')
+    y_pred = rfecv.predict(X_test)
+    print(f'Confusion Matrix:\n{confusion_matrix(y_test, y_pred)}\n')
+    print(f'Score of the classifier on test data:\n'
+          f'Accuracy  = {accuracy_score(y_test, y_pred)*100:.3f}%\n'
+          f'Precision = {precision_score(y_test, y_pred)*100:.3f}%\n'
+          f'Recall    = {recall_score(y_test, y_pred)*100:.3f}%\n'
+          f'F1 score  = {f1_score(y_test, y_pred)*100:.3f}%\n\n')
+
+    duration = time() - start
+    print('Operation took:', f'{duration:.2f} seconds.' if duration < 60 else f'{duration/60:.2f} minutes.')
+
+else:
+    print(f"\nModule imported : {__name__}")
