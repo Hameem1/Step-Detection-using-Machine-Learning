be2567b3e4f23718ae8224657a01c07dd4211bee
Hameem <hameemahmed.1@gmail.com>
2019-03-10 15:25:02 +0100

    Added functionality to import/export the trained model. Added ROC_AUC score. Added Disjoint testing functionality. Added generation of feature_ranking.csv Added plotting functionality for # of features vs Model Score using Plotly Added configuration variables

--- model/feature_selection_cv.py
+++ model/feature_selection_cv.py
@@ -1,49 +1,60 @@
 """
 This module implements Model Based Feature Selection (with cross-validated selection of best # of features).
 The model used is a Random Forest classifier.
-The model is trained during the feature selection phase and is tested at the end on test data from the best features.
+The model is trained during the feature selection phase and is tested at the end on test data using the best features.
 
 """
 
-# Todo: Generate a csv file containing the feature ranking
 # Todo: Combine the feature selection functionality of both models and rename feature_selection.py to model.py
 # Todo: Use model.py for training the model with selected features (all for max performance)
-# Todo: Generate the # of features vs score plot using plotly
-# Todo: Use scikit-learn for normalization
-# Todo: Test with disjoint datasets (e.g. train with ds_right and test on ds_left)
+# Todo: Try the "roc-auc" metric as well (Study first)
 # Todo: Organize results
-
+# Todo: Use scikit-learn for more flexible normalization
+# Todo: Add docstrings for the entire "model" package
+# Todo: Refactor the variable names involving paths, used in config.py
 
 # Imports
 import os
+import pickle
 import numpy as np
 import pandas as pd
 from time import time
+import plotly.offline as pyo
+import plotly.graph_objs as go
 import matplotlib.pyplot as plt
-from config import new_sensor_paths, DATA_PATH
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_selection import chi2, SelectKBest, RFECV
+from sklearn.feature_selection import RFECV
 from sklearn.model_selection import train_test_split, StratifiedKFold
-from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, f1_score, recall_score
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, f1_score, recall_score, roc_auc_score
+from config import new_sensor_paths, DATA_PATH, data_files_path, ROOT, \
+    TRAINED_MODEL_PATH, TRAINED_MODEL_NAME, TRAINED_MODEL_DIR
 
 
-# Globals
+# Configuration Variables
 # list of all feature labels + StepLabel
 cols = pd.read_csv(f'{new_sensor_paths[0]}\\{os.listdir(new_sensor_paths[0])[0]}', sep='\t', index_col=0).columns
 # Setting numpy print precision
 np.set_printoptions(precision=5)
 # no. of rows of dataset to be used
-row_count = 50000
+row_count = 5000
 # no. of Decision Trees per Random Forest
 RF_ESTIMATORS = 100
+# Test Data size (out of 1.0)
+TEST_SIZE = 0.5
 # Performance metric to optimize the model for
 SCORING = 'f1_weighted'
 # If True, the dataset is normalized before training
 DATA_NORMALIZATION = True
 # If True, a selected portion of the entire dataset is used for training (# of rows = row_count)
 DATA_REDUCE = True
-# Top t features to show
-t = 30
+# If True, generate a .csv file for the feature ranking
+GEN_RANKING_FILE = True
+# If True, a plot will be generated for the # of features used vs performance metric
+PLOT = True
+# Test on a separate dataset
+DISJOINT_TESTING = False
+# Path for disjoint test dataset
+TEST_DATA_PATH = f"{ROOT}\\Features_Dataset\\ds_left.csv"
 
 
 def normalize(data):
@@ -56,11 +67,39 @@ def normalize(data):
     return df_new
 
 
+def plot_n_features_vs_score(grid_scores, mp_lib=False):
+    x = list(range(1, len(grid_scores) + 1))
+    y = grid_scores
+    # Generating the plot trace
+    data = [go.Scatter(x=x, y=y, text=[f'{i * 100:.2f}%' for i in y],
+                       hoverinfo='text + x',
+                       marker=dict(color='green'),
+                       mode='lines',
+                       showlegend=False)]
+    # Defining the layout for the plot
+    layout = go.Layout(title='No. of Features used vs Model Score',
+                       xaxis=dict(title='# of features used'),
+                       yaxis=dict(title=SCORING),
+                       font=dict(family='arial', size=18, color='#000000'))
+
+    # Plotting the figure
+    fig = dict(data=data, layout=layout)
+    pyo.plot(fig, filename=f'{data_files_path}\\Number of features vs Model score' + '.html', auto_open=False)
+    print(f'>> File Generated : Number of features vs Model score' + '.html\n')
+
+    if mp_lib:
+        plt.figure()
+        plt.xlabel("Number of features selected")
+        plt.ylabel("Cross validation score (nb of correct classifications)")
+        plt.plot(range(1, len(rfecv.grid_scores_) + 1), rfecv.grid_scores_)
+        plt.show()
+
+
 if __name__ == '__main__':
     # starting timer
     start = time()
-    # loading in the entire actual dataset for one sensor
-    print(DATA_PATH)
+    # loading in the entire actual dataset
+    print(f'{DATA_PATH}\n')
     DATA = pd.read_csv(DATA_PATH, sep='\t', index_col=0)
     # Loading the relevant data (limiting # of rows)
     if DATA_REDUCE:
@@ -76,44 +115,76 @@ if __name__ == '__main__':
     X = data_matrix[:, 0:-1]
     y = data_matrix[:, -1]
     # Splitting the data into training and testing splits
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=10)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, random_state=101)
 
     # Feature selection
     # Recursive Feature Elimination/Model Based Feature Selection (with cross-validated selection of best # of features)
     model = RandomForestClassifier(n_estimators=RF_ESTIMATORS)
-    rfecv = RFECV(estimator=model, cv=StratifiedKFold(2), scoring=SCORING, n_jobs=-1)
-    print('>> Training the model\n')
+    rfecv = RFECV(estimator=model, cv=StratifiedKFold(2), scoring=SCORING, n_jobs=1)
+    print('>> Training the model & Performing feature ranking simultaneously\n')
     fit = rfecv.fit(X_train, y_train)
+    print('>> Model Trained!\n')
+    print('>> Feature Ranking complete!\n')
 
-    # summarize ranking
+    # Summarizing the ranking and generating a .csv file
     print(f"# of selected features:     {fit.n_features_}/{len(cols[:-1])}\n")
     feature_names = list(DATA.columns[0:-1][fit.support_])
-    print(f"Selected Features:\n{fit.support_}\n{feature_names}\n")
+    print(f"Selected Features:\n\n{feature_names}\n")
     ranking = pd.DataFrame({'rank': fit.ranking_, 'feature': DATA.columns[0:-1]})
     ranking = ranking.sort_values(by=['rank']).reset_index(drop=True)
-    top_t_features = list(ranking.feature[0:t])
-    print(f'Top {t} features list:\n{top_t_features}\n')
-    print(f"Feature Ranking:\n{fit.ranking_}\n{ranking}\n")
-    print(f"Optimal number of features : {rfecv.n_features_}")
-    # Plotting number of features VS. cross-validation scores
-    plt.figure()
-    plt.xlabel("Number of features selected")
-    plt.ylabel("Cross validation score (nb of correct classifications)")
-    plt.plot(range(1, len(rfecv.grid_scores_) + 1), rfecv.grid_scores_)
-    plt.show()
+    ranking.index = np.arange(1, len(ranking) + 1)
+    print(f"Optimal number of features : {rfecv.n_features_}\n")
+    # print(f"Feature Ranking:\n{fit.ranking_}\n{ranking}\n")
 
+    # Generating .csv file from feature ranking
+    ranking.to_csv(f'{data_files_path}\\feature ranking' + ".csv", sep="\t", index=True)
+    print(f'>> File generated : feature ranking.csv\n')
+
+    # Plotting number of features VS. cross-validation scores
+    if PLOT:
+        plot_n_features_vs_score(rfecv.grid_scores_)
+
+    # Testing the model
     print('>> Testing model\n')
-    print(f'Score of the classifier on test data = {rfecv.score(X_test, y_test)*100:.3f}%\n')
+    if DISJOINT_TESTING:
+        TEST_DATA = normalize(pd.read_csv(TEST_DATA_PATH, sep='\t', index_col=0)).values
+        X_test = TEST_DATA[:, 0:-1]
+        y_test = TEST_DATA[:, -1]
+
+    print(f'Performance metric used for model optimization : "{SCORING}"\n')
+    print(f'Score of the classifier on test data = {rfecv.score(X_test, y_test) * 100:.3f}%\n')
     y_pred = rfecv.predict(X_test)
     print(f'Confusion Matrix:\n{confusion_matrix(y_test, y_pred)}\n')
     print(f'Score of the classifier on test data:\n'
-          f'Accuracy  = {accuracy_score(y_test, y_pred)*100:.3f}%\n'
-          f'Precision = {precision_score(y_test, y_pred)*100:.3f}%\n'
-          f'Recall    = {recall_score(y_test, y_pred)*100:.3f}%\n'
-          f'F1 score  = {f1_score(y_test, y_pred)*100:.3f}%\n\n')
+          f'Accuracy  = {accuracy_score(y_test, y_pred) * 100:.3f}%\n'
+          f'Precision = {precision_score(y_test, y_pred) * 100:.3f}%\n'
+          f'Recall    = {recall_score(y_test, y_pred) * 100:.3f}%\n'
+          f'F1 score  = {f1_score(y_test, y_pred) * 100:.3f}%\n'
+          f'ROC_AUC   = {roc_auc_score(y_test, y_pred) * 100:.3f}%\n\n')
 
     duration = time() - start
-    print('Operation took:', f'{duration:.2f} seconds.' if duration < 60 else f'{duration/60:.2f} minutes.')
+    print('Operation took:', f'{duration:.2f} seconds.\n' if duration < 60 else f'{duration / 60:.2f} minutes.\n')
+
+    # Creating the directory for the trained model
+    if not os.path.exists(TRAINED_MODEL_PATH):
+        print(f'WARNING: The path does not exist. Creating new directory...\n{TRAINED_MODEL_PATH}\n')
+        os.mkdir(TRAINED_MODEL_PATH)
+    else:
+        print(f"Path for '{TRAINED_MODEL_DIR}' already exists!\n")
+
+    # Saving the model externally in TRAINED_MODEL_PATH
+    with open(f"{TRAINED_MODEL_PATH}\\{TRAINED_MODEL_NAME}", 'wb') as step_detection_model:
+        pickle.dump(rfecv, step_detection_model)
+    print(f'>> Model stored externally as "{TRAINED_MODEL_NAME}"\n')
+
 
 else:
-    print(f"\nModule imported : {__name__}")
+    print(f"\nModule imported : {__name__}\n")
+    # Loading the trained model
+    with open(f"{TRAINED_MODEL_PATH}\\{TRAINED_MODEL_NAME}", 'rb') as step_detection_model:
+        model = pickle.load(step_detection_model)
+    print('>> Model Imported.\n')
+    print("The following model is now available for testing:\n\n"
+          f"{model}\n\n"
+          f">> This model was trained on {model.n_features_} features.\n")
+
