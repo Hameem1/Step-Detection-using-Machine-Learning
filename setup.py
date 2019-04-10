885c695f8ce32c750172bc0796bd07e7b4bf2be0
Hameem <hameemahmed.1@gmail.com>
2019-04-11 01:54:42 +0200

    Replaced the os module with subprocess for bash commands

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python3
 
-import os
+import os, subprocess
 from pathlib import Path
 container_dir = Path("/StepDetectionProject")
 host_dir = Path(os.getcwd())
 
-# Building the image
-os.system(f"docker build -t step-detection:latest .")
-print(f"The Image has been created/pulled!\n")
+BUILD = False
+COMMAND = "bash"
+subprocess.call("clear")
+
+if BUILD:
+    # Building the image
+    subprocess.call("docker build -t step-detection:latest .", shell=True)
+    print(f"The Image has been created/pulled!\n")
 
 # Running the container from the image
 print(f"Running the container with this image...\n")
-os.system(f"docker run -it --name app "
-          f"-p 5000-5001:5000-5001 "
-          f"-v /.:{container_dir} "
-          f"step-detection:latest "
-          f"bash")
+subprocess.call(f"docker run -it --rm --name app "
+                f"-p 5000-5001:5000-5001 "
+                f"-v /.:{container_dir} "
+                f"step-detection:latest "
+                f"{COMMAND}", shell=True)
 
 print(f"\nThe container has been exited!\n")
 
