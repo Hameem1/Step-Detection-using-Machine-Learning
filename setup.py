b79601403b96660178aa8fbb915ea4811e984c7a
Hameem <hameemahmed.1@gmail.com>
2019-04-14 19:40:59 +0200

    added comments

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 
+# This script pulls the image from dockerhub & runs the container with the image, using docker-compose.
+# The container is removed upon exiting from it.
+
 import os, subprocess
-from pathlib import Path
-container_dir = Path("/StepDetectionProject")
+
+IMAGE_NAME = "hameem/step-detection:dev"
 
 subprocess.call("clear")
 
-subprocess.call("docker pull hameem/step-detection:dev", shell=True)
-# print(f"The Image has been created/pulled!\n")
+# Pulling the image from dockerhub
+subprocess.call(f"docker pull {IMAGE_NAME}", shell=True)
+print(f"The Image has been pulled!\n")
 
-# Running the container from the image
-print(f"Running the container with this image...\n")
+# Running a container from the image
+print(f"Running the container with image : {IMAGE_NAME}\n")
 subprocess.call("docker-compose run --rm --service-ports application", shell=True)
 
 print(f"\nThe container has been exited!\n")
-
